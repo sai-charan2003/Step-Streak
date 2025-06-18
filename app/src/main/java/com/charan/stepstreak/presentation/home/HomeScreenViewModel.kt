cf39ade618eca89504d859ae5228e86fae98a8c5
Sai Charan <saicharannallapaneni@gmail.com>
2025-06-18 18:08:38 +0530

    Bug fixes

--- app/src/main/java/com/charan/stepstreak/presentation/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/charan/stepstreak/presentation/home/HomeScreenViewModel.kt
@@ -112,6 +112,7 @@ class HomeScreenViewModel @Inject constructor(
         when(event){
             HomeEvent.OnRefresh -> {
                 fetchSteps()
+                getCurrentTargetSteps()
             }
         }
 
